823dcb54aff38c1e7e484f8db7f414abc488ce9d
Amaury Carrade <amaury@carrade.eu>
2021-04-16 01:42:19 +0200

    Added info command

--- src/main/java/me/cassayre/florian/hawk/commands/InfoCommand.java
+++ src/main/java/me/cassayre/florian/hawk/commands/InfoCommand.java
@@ -37,11 +37,100 @@ package me.cassayre.florian.hawk.commands;
 import fr.zcraft.quartzlib.components.commands.Command;
 import fr.zcraft.quartzlib.components.commands.CommandException;
 import fr.zcraft.quartzlib.components.commands.CommandInfo;
+import fr.zcraft.quartzlib.components.i18n.I;
+import fr.zcraft.quartzlib.components.rawtext.RawText;
+import fr.zcraft.quartzlib.components.rawtext.RawTextPart;
+import me.cassayre.florian.hawk.Hawk;
+import me.cassayre.florian.hawk.report.Report;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 @CommandInfo(name = "info")
 public class InfoCommand extends Command {
     @Override
     protected void run() throws CommandException {
+        final Report report = Hawk.get().getReport();
 
+        if (report == null) {
+            info(I.t("There are no reports being recorded."));
+            return;
+        }
+
+        final int startedMinutesAgo = (int) ((System.currentTimeMillis() - report.getStartDate()) / 60000);
+
+        final int damages = report.getDamages().size();
+        final int heals = report.getHeals().size();
+        final int events = report.getEvents().size();
+        final int all = damages + heals + events;
+
+        final Player player = sender instanceof Player ? playerSender() : null;
+
+        final RawTextPart<?> infos = new RawText().color(ChatColor.GRAY)
+            .then(report.getTitle())
+                .style(ChatColor.GREEN, ChatColor.BOLD)
+            .then("\n")
+            .then(I.tln(player, "Started a minute ago", "Started {0} minutes ago", startedMinutesAgo))
+                .color(ChatColor.DARK_GRAY)
+            .then("\n\n")
+            .then(I.tln(player, "{0} player", "{0} players", report.getPlayers().size()))
+            .then(" - ")
+            .then(I.tln(player, "{0} tracked", "{0} tracked", report.getTrackedPlayers().size()))
+            .then("\n");
+
+        if (report.getTeams().size() != 0) {
+            infos.then(I.tln(player, "{0} team", "{0} teams", report.getTeams().size())).then("\n");
+        }
+
+        infos
+            .then(I.tln(player, "{0} record", "{0} records", all))
+            .then(" (")
+            .then(I.tln(player, "{0} damage", "{0} damages", damages)).then(", ")
+            .then(I.tln(player, "{0} heal", "{0} heals", heals)).then(", ")
+            .then(I.tln(player, "{0} event", "{0} events", events))
+            .then(")");
+
+        if (report.isAutoTrackingNewPlayers() || report.isStoppingTrackOnDeath() || report.isStoppingTrackOnDisconnection()) {
+            infos.then("\n\n");
+        }
+
+        if (report.isAutoTrackingNewPlayers()) {
+            infos.then(I.tl(player, "Tracking new players if they join")).color(ChatColor.DARK_GRAY);
+        }
+
+        if (report.isStoppingTrackOnDeath()) {
+            infos.then(I.tl(player, "Stopping track on death")).color(ChatColor.DARK_GRAY);
+        }
+
+        if (report.isStoppingTrackOnDisconnection()) {
+            infos.then(I.tl(player, "Stopping track on disconnection")).color(ChatColor.DARK_GRAY);
+        }
+
+        if (sender instanceof Player) {
+            send(
+                new RawText(I.tl(player, "Hawk is currently recording a game session."))
+                        .color(ChatColor.GREEN)
+                        .hover(infos)
+                    .then(" ")
+                    .then(I.tl(player, "Hover this message for details."))
+                        .color(ChatColor.GRAY)
+                        .hover(infos)
+                    .build()
+            );
+        } else {
+            send(
+                new RawText(I.tl(player, "Hawk is currently recording a game session."))
+                    .color(ChatColor.GREEN)
+                    .hover(infos)
+            );
+
+            send(infos.build());
+        }
+
+        send(
+            new RawText(I.tl(player, "To stop and publish this session, run {cc}/hawk stop{gray}."))
+                .color(ChatColor.GRAY)
+                .suggest(StopCommand.class)
+                .hover(I.tl(player, "Click here to pre-fill the command"))
+        );
     }
 }
