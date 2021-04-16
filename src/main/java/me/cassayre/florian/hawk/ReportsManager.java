76e40a37711bc229a596f835808742294c7d1694
Amaury Carrade <amaury@carrade.eu>
2021-04-16 02:20:40 +0200

    Fixed javadoc (again)

--- src/main/java/me/cassayre/florian/hawk/ReportsManager.java
+++ src/main/java/me/cassayre/florian/hawk/ReportsManager.java
@@ -76,7 +76,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 /**
- * The reports manager: registers, saves, manages export & backups of,
+ * The reports manager: registers, saves, manages export &amp; backups of,
  * all reports.
  * <p>
  * If you use this without the plugin, by shading the library, you must
